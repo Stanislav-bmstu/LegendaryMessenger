bfbcc0e5ded3b49f34e138dc5492f0500414ecec
Stanislav <stanislav.bmstu@mail.ru>
2015-10-11 00:31:19 +0300

    Реализовано два активити. Первое - заставка (пропадает через 2 секунды)

---
+++ app/src/main/java/com/bmstu/stanislav/legendarymessenger/MainActivity.java
@@ -0,0 +1,57 @@
+package com.bmstu.stanislav.legendarymessenger;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread splash_time = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(MainActivity.this, ListActivity.class));
+                    finish();
+                }
+            }
+        };
+        splash_time.start();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
