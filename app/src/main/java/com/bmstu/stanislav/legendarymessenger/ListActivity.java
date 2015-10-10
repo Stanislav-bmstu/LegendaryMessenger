93aa2b1bf081ab5f3d527a47f451795a5f15f8d0
Stanislav <stanislav.bmstu@mail.ru>
2015-10-11 02:31:11 +0300

    Во вторую активити добавлен ListView. ListView наполнен числами в строковом представлении (1 - 1000)

--- app/src/main/java/com/bmstu/stanislav/legendarymessenger/ListActivity.java
+++ app/src/main/java/com/bmstu/stanislav/legendarymessenger/ListActivity.java
@@ -2,6 +2,12 @@ package com.bmstu.stanislav.legendarymessenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -9,5 +15,75 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, initData());
+
+        listView.setAdapter(adapter);
+    }
+
+    private LinkedList<String> initData() {
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        List<String> units = new ArrayList<>();
+        List<String> dozens = new ArrayList<>();
+        List<String> hundreds = new ArrayList<>();
+
+        units.add("один");
+        units.add("два");
+        units.add("три");
+        units.add("четыре");
+        units.add("пять");
+        units.add("шесть");
+        units.add("семь");
+        units.add("восемь");
+        units.add("девять");
+        units.add("десять");
+        units.add("одинадцать");
+        units.add("двенадцать");
+        units.add("тринадцать");
+        units.add("четырнадцать");
+        units.add("пятнадцать");
+        units.add("шестнадцать");
+        units.add("семнадцать");
+        units.add("восемнадцать");
+        units.add("девятнадцать");
+
+        dozens.add("двадцать");
+        dozens.add("тридцать");
+        dozens.add("сорок");
+        dozens.add("пятьдесят");
+        dozens.add("шестьдесят");
+        dozens.add("семьдесят");
+        dozens.add("восемьдесят");
+        dozens.add("девяносто");
+
+        hundreds.add("сто");
+        hundreds.add("двести");
+        hundreds.add("триста");
+        hundreds.add("четыреста");
+        hundreds.add("пятьсот");
+        hundreds.add("шестьсот");
+        hundreds.add("семьсот");
+        hundreds.add("восемьсот");
+        hundreds.add("девятьсот");
+        hundreds.add("тысяча");
+
+        for (int i = -1; i < 9; i++) {
+            String hundred = (i < 0) ? "" : hundreds.get(i) + " ";
+            for (int j = -1; j < 8; j++) {
+                String dozen = (j < 0) ? "" : dozens.get(j) + " ";
+                int max = (j < 0) ? 19 : 9;
+                for (int k = 0; k < max; k++) {
+                    String unit = units.get(k);
+                    linkedList.add(hundred + dozen + unit);
+                }
+                if (j < 7) linkedList.add(hundred + dozens.get(j + 1));
+            }
+            linkedList.add(hundreds.get(i + 1));
+        }
+
+        return linkedList;
     }
 }
